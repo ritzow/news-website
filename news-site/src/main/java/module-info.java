34f0278e34a7292f4d4fa93b8f48fe06b9a9bafd
Solomon Ritzow <>
2021-11-29 21:54:34 -0800

    Remove direct org.hsqldb module dependency
    Not necessary, since it is included by java.sql service interface

--- news-site/src/main/java/module-info.java
+++ news-site/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module net.ritzow.news {
 	requires net.ritzow.jetstart;
 	requires com.j2html;
 	requires org.commonmark;
-	requires org.hsqldb;
 	requires org.hsqldb.sqltool;
 	requires java.sql;
 	requires com.zaxxer.hikari;
