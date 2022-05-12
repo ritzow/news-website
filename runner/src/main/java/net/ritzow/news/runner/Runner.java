7b8c605a6e5ec7f19bb7ffa6eac161bc02d7bba6
Solomon Ritzow <>
2022-05-11 23:28:52 -0700

    Execute package phase before runner:run goal

--- runner/src/main/java/net/ritzow/news/runner/Runner.java
+++ runner/src/main/java/net/ritzow/news/runner/Runner.java
@@ -5,12 +5,10 @@ import java.io.IOException;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.plugins.annotations.*;
 
 @SuppressWarnings("unused")
+@Execute(phase = LifecyclePhase.PACKAGE)
 @Mojo(
 	name = "run", 
 	defaultPhase = LifecyclePhase.NONE, 
@@ -19,7 +17,7 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 public class Runner extends AbstractMojo {
 	
 	@SuppressWarnings("unused")
-	@Parameter( defaultValue = "${session}", readonly = true, required = true)
+	@Parameter(defaultValue = "${session}", readonly = true, required = true)
 	private MavenSession session;
 	
 	@SuppressWarnings("unused")
