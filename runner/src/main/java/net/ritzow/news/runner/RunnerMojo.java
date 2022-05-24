87d02c07ca818d3ef1d53be346afc4ec4af2b12c
Solomon Ritzow <>
2022-05-23 21:27:07 -0700

    Make RunnerMojo more robust
    Handle more error cases.
    Destroy leftover child processes.
    Also use Process reader methods instead of Scanner.

--- runner/src/main/java/net/ritzow/news/runner/RunnerMojo.java
+++ runner/src/main/java/net/ritzow/news/runner/RunnerMojo.java
@@ -2,7 +2,10 @@ package net.ritzow.news.runner;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoFailureException;
@@ -57,41 +60,75 @@ public class RunnerMojo extends AbstractMojo {
 				+ " when stopped."
 			);
 			
-			new Thread(() -> {
-				try(var scan = new Scanner(process.getInputStream())) {
-					while(scan.hasNextLine()) {
-						getLog().info(scan.nextLine());
-					}
+			var stdout = new Thread(() -> {
+				try(var scan = process.inputReader(StandardCharsets.UTF_8)) {
+					scan.lines().forEachOrdered(getLog()::info);
+				} catch(IOException e) {
+					throw new RuntimeException(e);
 				}
-			}).start();
+			});
+			stdout.setDaemon(true);
+			stdout.start();
 
-			new Thread(() -> {
-				try(var scan = new Scanner(process.getErrorStream())) {
-					while(scan.hasNextLine()) {
-						getLog().warn(scan.nextLine());
-					}
+			var stderr = new Thread(() -> {
+				try(var scan = process.errorReader(StandardCharsets.UTF_8)) {
+					scan.lines().forEachOrdered(getLog()::warn);
+				} catch(IOException e) {
+					throw new RuntimeException(e);
 				}
-			}).start();
+			});
+			stderr.setDaemon(true);
+			stderr.start();
 
 			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-				getLog().info("Shutting down " + process.info()
-					.commandLine()
-					.orElseGet(() -> "PID " + process.pid()));
-				process.destroy();
-//				try {
-//					process.waitFor();
-//				} catch(InterruptedException e) {
-//					e.printStackTrace();
-//				}
+				if(process.isAlive()) {
+					getLog().info("Shutting down " + process.info()
+						.commandLine().orElseGet(() -> "PID " + process.pid()));
+
+					try {
+						process.destroy();
+						int status = exitApplication(process);
+						if(status != 0) {
+							getLog().error("Application exited with exit code " + status);
+						}
+					} catch(InterruptedException e) {
+						getLog().error("Interrupted while waiting for application termination");
+					}
+				}
 			}));
 
-			int status = process.waitFor();
-
-			if(status != 0) {
-				throw new MojoFailureException("Program exited with return status " + status);
+			try {
+				int status = exitApplication(process);
+				if(status != 0) {
+					throw new MojoFailureException("Application exited with exit code " + status);
+				}
+			} catch(InterruptedException e) {
+				getLog().error("Interrupted while waiting for application termination");
 			}
-		} catch(IOException | InterruptedException e) {
+		} catch(IOException e) {
 			getLog().error(e);
 		}
+	}
+	
+	private int exitApplication(Process app) throws InterruptedException {
+		int status = app.waitFor();
+		app.descendants().forEach(ProcessHandle::destroy);
+		app.descendants().forEach(process -> {
+			process.onExit().orTimeout(5, TimeUnit.SECONDS).whenComplete((proc, ex) -> {
+				if(ex != null) {
+					if(ex instanceof InterruptedException || ex instanceof ExecutionException) {
+						getLog().error("Failed to wait for application descendant PID " + process.pid() + " to exit");			
+					} else if(ex instanceof TimeoutException e) {
+						if(process.destroyForcibly()) {
+							getLog().warn("Forcibly exited descendant PID " + process.pid());	
+						} else {
+							getLog().error("Failed to forcibly exit descendant PID " + process.pid());
+						}
+					}
+				}
+			});
+		});
+		app.descendants().forEach(process -> process.onExit().join());
+		return status;
 	}
 }
