48ffedd3a64ea9d02bb08a0f7f67595767548052
Solomon Ritzow <>
2022-05-23 21:31:38 -0700

    Make keystorePassword optional for self-signed certs

--- news-site/src/main/java/net/ritzow/news/run/RunSite.java
+++ news-site/src/main/java/net/ritzow/news/run/RunSite.java
@@ -1,17 +1,20 @@
 package net.ritzow.news.run;
 
+import java.math.BigInteger;
 import java.net.InetAddress;
 import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Objects;
 import java.util.Set;
 import net.ritzow.news.Certs;
 import net.ritzow.news.NewsSite;
 
 public class RunSite {
 	public static void main(String[] args) throws Exception {
-		
 		String host = System.getProperty("hostname");
 		String organization = System.getProperty("organization");
-		String password = System.getProperty("keystorePassword");
+		String password = Objects.requireNonNullElseGet(System.getProperty("keystorePassword"), RunSite::generatePassword);
 		
 		var cert = System.getProperty("keystore") == null ? Certs.selfSigned(host, organization, password.toCharArray())
 			: Certs.loadPkcs12(Path.of(System.getProperty("keystore")), password.toCharArray());
@@ -23,5 +26,13 @@ public class RunSite {
 			Set.of("127.0.0.1", "[::1]", host),
 			InetAddress.getByName("0.0.0.0")
 		);
+	}
+
+	private static String generatePassword() {
+		try {
+			return new BigInteger(512, SecureRandom.getInstanceStrong()).toString(Character.MAX_RADIX);
+		} catch(NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
